3535fa8a486df2f837ffb67638590971b5575d2b
josueliasl <josue.elias.leyva@gmail.com>
2026-03-17 23:13:47 -0600

    yes!

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const expressLayouts = require("express-ejs-layouts")
 const path = require("path")
 require("dotenv").config()
 
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities/")  // ADD THIS LINE for error handler
+
 const app = express()
 
 /* Routes */
@@ -23,7 +27,32 @@ app.set("layout", "layouts/layout")
 app.use(express.static(path.join(__dirname, "public")))
 
 /* Routes */
+// Index route
+app.get("/", baseController.buildHome)
+
+// Inventory routes
+app.use("/inv", inventoryRoute)
+
 app.use("/", staticRoutes)
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+/***************************
+ * Express Error Handler
+ * Place after all other middleware
+ **************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: ${req.originalUrl}: ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
 
 /* Server */
 const port = process.env.PORT || 5500
